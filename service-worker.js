66537e0102668ef11948c82147a79b22f21a5fa9
SiyuYe <mia791003@gmail.com>
2026-07-12 00:33:27 +0800

    CMS_V7_8_4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE_NAME='xxy-cms-v781';
+// CMS v7.8.3 cache refresh
+const CACHE_NAME='xxy-cms-v784';
 const CORE_ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./data/site-config.json','./data/home.json','./data/pages.json','./data/artworks.json','./data/exhibitions.json','./data/images-manifest.json','./about.html','./gallery.html','./works.html','./exhibitions.html','./history.html','./contact.html','./assets/icons/icon-192.png','./assets/icons/icon-512.png','./assets/images/art-placeholder-clean.svg','./assets/images/header-banner.webp','./api/cms.js'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(CORE_ASSETS).catch(()=>{})))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
